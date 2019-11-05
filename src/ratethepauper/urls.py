c635b8b37c9353f4969cfc8e4be5d38c604cd34f
Iglohut <Steven.Smits@outlook.com>
2019-11-05 13:01:16 +0100

    debug FALSE, mediaurls

--- src/ratethepauper/urls.py
+++ src/ratethepauper/urls.py
@@ -17,6 +17,8 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.conf.urls import url
+from django.views.static import serve
 
 from pages.views import (
     home_view,
@@ -42,3 +44,8 @@ if settings.DEBUG:
                           document_root=settings.STATIC_ROOT)
     urlpatterns += static(settings.MEDIA_URL,
                           document_root=settings.MEDIA_ROOT)
+else:
+    urlpatterns += url(r'^media/(?P<path>.*)$', serve,
+                       {'document_root': settings.MEDIA_ROOT}),
+    urlpatterns += url(r'^static/(?P<path>.*)$', serve,
+                       {'document_root': settings.STATIC_ROOT}),
